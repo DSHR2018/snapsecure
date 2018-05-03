b7d379da51efafb7761315fbecdbb6474a057176
harlo <harlo.holmes@gmail.com>
2018-05-03 10:42:17 -0400

    more reorg

--- html/snapsecure.js
+++ html/snapsecure.js
@@ -2,3 +2,19 @@ function login() {
 	document.getElementById("login").style.display = "none";
 	document.getElementById('input').style.display = "block";
 }
+
+var video = document.querySelector("#videoElement");
+var mediaStream = HTMLMediaElement.srcObject;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+if (navigator.getUserMedia) {       
+ navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+ video.src = window.URL.createObjectURL(stream);
+ video.play();
+ });
+}
+
+function videoError() {
+	// do something
+}
